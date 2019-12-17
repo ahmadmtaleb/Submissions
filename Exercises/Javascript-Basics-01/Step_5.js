4f90dabea3796077feb621cd4d0310e343c655a2
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2019-12-17 11:42:51 +0200

    Completed Step_5

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function display1() {
+  var n = document.getElementById("first_number");
+  var s = document.getElementById("second_number");
+  var message = "The result is: " + n.value * s.value;
+  alert(message);
+}
