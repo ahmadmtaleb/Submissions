f0b28629ea83e492e3bd76446d35e3159f6e61a2
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-01-14 14:43:25 +0200

    Weather API: htnl/css 2

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import clear from "./img/weather-icons/clear.svg";
+import cloudy from "./img/weather-icons/cloudy.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
+import snow from "./img/weather-icons/snow.svg";
+import storm from "./img/weather-icons/storm.svg";
+import unknown from "./img/weather-icons/unknown.svg";
 
 class App extends Component {
   constructor(props) {
@@ -18,59 +28,56 @@ class App extends Component {
         </header>
         <main className="app__main">
           <div class="today_weather">
-            <img src="https://placebear.com/150/150" alt="bear"></img>
+            <img src={storm} alt="storm icon" />
             <p id="status">status</p>
-            <div class=" today2">
-              <p>
-                <strong>Temperature</strong>
-              </p>
+
+            <div class="today1">
+              <p class="strong">Temperature</p>
               <p>10 C</p>
             </div>
+
             <div class="today2">
-              <p>
-                <strong>humidity </strong>
-              </p>
+              <p class="strong">Humidity</p>
               <p>10</p>
-              <p>
-                <strong>pressure </strong>
-              </p>
+              <p class="strong">Pressure</p>
               <p>76</p>
             </div>
           </div>
+
           <div class="container_second">
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={clear} alt="storm icon" />
               <p>temp</p>
             </section>
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={snow} alt="storm icon" />
               <p>temp</p>
             </section>
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={rain} alt="storm icon" />
               <p>temp</p>
             </section>
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={fog} alt="storm icon" />
               <p>temp</p>
             </section>
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={cloudy} alt="storm icon" />
               <p>temp</p>
             </section>
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={partlycloudy} alt="storm icon" />
               <p>temp</p>
             </section>
             <section class="column">
               <p>hour</p>
-              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <img src={mostlycloudy} alt="storm icon" />
               <p>temp</p>
             </section>
           </div>
