2af2508dc7b6c75dd35459e5bd4db871e2b335aa
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-01-14 11:42:33 +0200

    Weather API: html/css

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -10,8 +10,71 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header"></header>
-        <main className="app__main">Say my name, beautiful blank page :p</main>
+        <header className="app__header">
+          <form action="/actionpage.">
+            <input type="text" name="city" placeholder="city" id="city"></input>
+            <input type="button" value="Find weather" id="find"></input>
+          </form>
+        </header>
+        <main className="app__main">
+          <div class="today_weather">
+            <img src="https://placebear.com/150/150" alt="bear"></img>
+            <p id="status">status</p>
+            <div class=" today2">
+              <p>
+                <strong>Temperature</strong>
+              </p>
+              <p>10 C</p>
+            </div>
+            <div class="today2">
+              <p>
+                <strong>humidity </strong>
+              </p>
+              <p>10</p>
+              <p>
+                <strong>pressure </strong>
+              </p>
+              <p>76</p>
+            </div>
+          </div>
+          <div class="container_second">
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+            <section class="column">
+              <p>hour</p>
+              <img src="https://placebear.com/100/100" alt="bear"></img>
+              <p>temp</p>
+            </section>
+          </div>
+        </main>
       </div>
     );
   }
