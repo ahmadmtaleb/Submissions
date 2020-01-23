4fc631216ef91c92c27a2a241bee490d2b10769e
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-01-23 23:38:19 +0200

    Step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -11,6 +11,33 @@ app.get("/test", (req, res) => {
 app.get("/time", (req, res) => {
   res.send({ status: 200, message: new Date() });
 });
+app.get("/hello/:id", (req, res) => {
+  res.send({ status: 200, message: "Hello, " + req.params.id });
+});
+
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+
+  if (typeof search != "undefined") {
+    const response = {
+      status: 200,
+      message: "ok",
+      data: search
+    };
+
+    res.send(response);
+  } else {
+    const response = {
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    };
+
+    res.status(500);
+    res.send(response);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
