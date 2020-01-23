d19993264cadcf1a94f0ab7b18b63e26860c7346
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-01-23 12:27:48 +0200

    Step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
 app.get("/", (req, res) => {
   res.send("OK");
+});
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  res.send({ status: 200, message: new Date() });
 });
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
