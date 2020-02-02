948250108f2f5ff8b9febe2daa65f6dfdad3d6af
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-02-02 18:09:30 +0200

    adding ssh key

--- Exercises/Javascript-Basics-03/step 1/step_1.js
+++ Exercises/Javascript-Basics-03/step 1/step_1.js
@@ -1,4 +1,4 @@
 function boxDisplay()
     {
-        document.getElementById('image1').style.border = '3px solid red';
+        document.getElementById('image1').style.border = '0px solid red';
     }
