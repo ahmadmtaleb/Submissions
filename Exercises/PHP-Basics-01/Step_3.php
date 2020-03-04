7ec87d6f009abcc496143df086358f066b27eef3
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-03-04 12:06:04 +0200

    Step_3 completed

---
+++ Exercises/PHP-Basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name ="MOE";
+$email = "moe@gmail.com";
+echo "My name is ${name}, and my email is ${email}\n";
+$height = 10;
+$width = 15;
+$area = $height * $width;
+echo "the area is equal to ${area}\n";
+?>
