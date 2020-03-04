0378196b32e14d5fa03af8ce2ab396b5f846745b
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-03-04 12:15:24 +0200

    Step_4 completed

---
+++ Exercises/PHP-Basics-01/Step_4.php
@@ -0,0 +1,11 @@
+<?php
+$name = "MOE";
+$position = "blah";
+$git_url = "blah@blouh.com";
+echo "My name is ".$name." I'm a ".$position." my url git is ".$git_url;
+echo "\n";
+echo "My name is ${name} I'm a ${position} my url git is ${git_url}";
+echo "\n";
+echo 'My name is '.$name.' I\'m a '.$position.' my url git is '.$git_url;
+echo "\n";
+?>
