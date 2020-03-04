f0f7adec44c4f8b7209d177f417f41713883dcb3
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-03-04 12:26:37 +0200

    Step5 completed

---
+++ Exercises/PHP-Basics-01/Step_5.php
@@ -0,0 +1,10 @@
+<?php
+$course_name = "mohamad";
+$enrolled_students= 2;
+$price  = 1.2;
+$on_discount = true;
+echo "Course title: $course_name
+Enrolled Students: $enrolled_students
+Course price: $price $ USD
+Course on discount: ", $on_discount ? 'yes' : 'no', " ";
+?>
