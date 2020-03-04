514c3b5c28982f8e64726cccd7427dbe6400c310
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-03-04 12:51:39 +0200

    step 8 and 9 completed

---
+++ Exercises/PHP-Basics-01/Step_8_b.php
@@ -0,0 +1,8 @@
+<?php
+$array = [
+    "eggs" => ['balade', 'mazere3'],
+    "milk"=> ['Fresh', 'Taanayel'], 
+    "water-pack"=> ['Tanoureen', 'Reem']
+]; 
+echo "Hey Sir, Please I need 1 pack of ". $array['eggs'][0] ." eggs and 3 ".$array['water-pack'][1]." Water Pack.";
+?>
---
+++ Exercises/PHP-Basics-01/Step_9.php
@@ -0,0 +1,7 @@
+<?php
+$name = "moe";
+var_dump($name);
+$num = 1564531;
+var_dump($num);
+
+?>
